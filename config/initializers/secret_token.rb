f50ae10c0cc0f236d2cee99355538b7914828546
alanvp <apvanpelt@yahoo.com>
2014-02-04 17:49:56 -0800

    adding heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Accountible::Application.config.secret_key_base = 'f295fb2e1ca993569ba8447f4767b52e6318d0650eb7ab1494497d31a2c856f417e9f164196aa0a05ee2f17fb1f396a1c013f53356fe2b29f829030fb50501ab'
+Accountible::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
