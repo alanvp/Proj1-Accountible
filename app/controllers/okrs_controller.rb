816ad91e28acff88cc5f4c3b0d12f2fdccf365e5
alanvp <apvanpelt@yahoo.com>
2014-02-04 17:01:36 -0800

    got edit and delete OKRs working

--- app/controllers/okrs_controller.rb
+++ app/controllers/okrs_controller.rb
@@ -6,13 +6,29 @@ class OkrsController < ApplicationController
   end
 
   def show
-   
   	id = params[:format]
-
   	@teammate = User.find(id)
+  end
+
+  def my_okrs
 
   end
 
-  def delete
+  def edit
+    @okr = Okr.find(params[:id].to_i)
+  end
+
+  def update
+    @okr = Okr.find(params[:id])
+    @okr.update_attributes(params.require(:okr).permit(:id, :objective, :kr1, :kr2, :kr3, :kr4, :kr5))
+    render :my_okrs
+
+  end
+
+  def destroy
+    @okr = Okr.find(params[:id])
+    @okr.delete
+    render :my_okrs
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Accountible::Application.routes.draw do
   get "okrs/create"
   get "okrs/show", to: 'okrs#show', as: :show_okr
   get "okrs/delete"
-
+  get "okrs/my_okrs", to: 'okrs#my_okrs'
+  get "okrs/:id/edit", to: 'okrs#edit', as: :edit_okr
+  patch "okrs/:id/edit", to: 'okrs#update', as: :update_okr
+  delete "okrs/:id/edit", to: 'okr#destroy', as: :delete_okr
 
   get '/signup' => 'users#new'
   match '/signout', to: 'sessions#destroy', via: :delete
